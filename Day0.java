1d93c3b94a0988acec56d6429ea9b73e29fc2767
HTI-shivanidotcom <142248478+HTI-shivanidotcom@users.noreply.github.com>
2023-08-14 23:36:28 +0530

    Assignment Completed

---
+++ Day0.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Day0
+{
+  public static void main (String[]args)
+  {
+    Scanner sc = new Scanner (System.in);
+      System.out.println ("Enter first Number");
+        int a = sc.nextInt ();
+        System.out.println ("Enter second Number");
+        int b = sc.nextInt ();
+        System.out.println("The result "+(a+b));
+
+  }
+}
